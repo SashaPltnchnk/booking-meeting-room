a715bbc72b935a158b53ca600ce009cf83137d94
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-05-30 08:58:03 +0300

    some fixes

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -3,6 +3,7 @@ import { createDriver } from 'redux-saga-requests-axios'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://web-ninjas.net/'
+axios.defaults.headers.Authorization = localStorage.getItem('token')
 
 export default function* rootSaga() {
   yield createRequestInstance({ driver: createDriver(axios) })
